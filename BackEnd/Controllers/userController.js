2cd4fbd57dd1c248ce33d992c85b29c012ff464b
Shakil <aburaihancse066@gmail.com>
2022-10-16 23:08:39 +0600

    add logout

--- BackEnd/Controllers/userController.js
+++ BackEnd/Controllers/userController.js
@@ -68,4 +68,22 @@ exports.userLogin = async (req, res) => {
 // User logout
 exports.userLogout = async (req, res) => {
     const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+        res.status(400).json({
+            success: false,
+            message: "User is not found with this email !!"
+        })
+    }
+
+    res.status(200).cookie('token', null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    }).json({
+        success: true,
+        message: "Log out successful !!"
+    })
+
 }
