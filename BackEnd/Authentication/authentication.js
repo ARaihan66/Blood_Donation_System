38e79f08247778b75819b77a05851f7c0f96cd4b
Shakil <aburaihancse066@gmail.com>
2022-10-17 01:26:19 +0600

    add authentication

---
+++ BackEnd/Authentication/authentication.js
@@ -0,0 +1,30 @@
+const User = require('../Models/UserModel');
+const jwt = require('jsonwebtoken');
+
+exports.authenticatedUser = async (req, res, next) => {
+    const token = req.cookie;
+    if (!token) {
+        res.status(403).json({
+            success: false,
+            message: "Please login first !!"
+        })
+    }
+
+    const varification = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.user = await User.findById(verification.id);
+    next();
+}
+
+
+// Admin role
+exports.authorizeRole = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            res.status(400).json({
+                message: "User unable to access the resources!!!",
+            })
+        }
+        next();
+    }
+}
--- BackEnd/Routers/userRouter.js
+++ BackEnd/Routers/userRouter.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { createAccount } = require("../Controllers/userController");
+const { authenticatedUser, authorizeRole } = require('../Authentication/authentication');
+const { createAccount, userLogin, userLogout } = require("../Controllers/userController");
 const router = express.Router();
 
 router.route('/create/account')
     .post(createAccount)
+
+router.route('/login')
+    .post(authenticatedUser, userLogin)
+
+router.route('/logout')
+    .post(authenticatedUser, userLogout)
 
 
 module.exports = router;
