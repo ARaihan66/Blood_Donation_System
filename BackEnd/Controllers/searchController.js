b4b9ed4626dc88578dd060246b2f136bf8ff8ded
Shakil <aburaihancse066@gmail.com>
2022-10-23 02:19:05 +0600

    add pagonation in searchController

--- BackEnd/Controllers/searchController.js
+++ BackEnd/Controllers/searchController.js
@@ -3,8 +3,14 @@ const User = require('../Models/UserModel');
 // Get specific blood group
 exports.specificBloodHolder = async (req, res) => {
     const blood_group = req.body.blood_group;
+    let { page, limit, sort } = req.query;
 
-    const user = await User.find({ blood_group: blood_group });
+    if (!page) page = 1;
+    if (!limit) limit = 5;
+
+    const skip = (page - 1) * 10
+
+    const user = await User.find({ blood_group: blood_group }).sort({ [sort]: -1 }).skip(skip).limit(limit);
     if (user.length == 0) {
         return res.status(400).json({
             success: false,
@@ -15,6 +21,7 @@ exports.specificBloodHolder = async (req, res) => {
 
     res.status(200).json({
         success: true,
+        Page: page,
         User: user
     })
 
